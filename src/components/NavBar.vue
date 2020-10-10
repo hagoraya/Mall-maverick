db1680c8c11ae9488dd6ba8602561121ea6f049e
Harpreet Goraya <harpreetgoraya95@gmail.com>
2020-10-10 17:14:24 -0400

    added navbar styles

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,17 +5,34 @@
       </b-navbar-brand>
       <b-navbar-toggle target="collapse-area"></b-navbar-toggle>
       <b-collapse id="collapse-area" is-nav>
-        <b-navbar-nav>
-          <b-nav-item>Navbar text</b-nav-item>
+        <div class="right-nav-items">
+          <b-navbar-nav>
+          <img src="./../assets/Menu Icon.svg" alt="Menu">
         </b-navbar-nav>
+        <b-navbar-nav>
+          <span class="navbar-text" id="open-now">Stores Directory</span>
+        </b-navbar-nav>
+        </div>
+
         <b-navbar-nav class="d-none d-md-block mx-auto">
           <b-nav-text>
             <h4 class="company-text">Mall Maverick</h4>
           </b-nav-text>
         </b-navbar-nav>
         <b-navbar-nav>
-          <b-nav-item>Navbar text 2</b-nav-item>
-        <img src="src\Views\HomePage\Assets\Search Button.svg" alt="" />
+
+        <div class="right-navbar-content"> 
+         <b-nav-text>
+            <span class="navbar-text" id="open-now">Open Now: 9Am 9Pm</span>
+          </b-nav-text>
+          <b-nav-text>
+            <span class="navbar-text" id="open-now">Contact</span>
+          </b-nav-text>
+        <b-nav-text>
+            <img src="./../assets/SearchButton.svg" alt="Search Button">
+          </b-nav-text>
+      </div>
+
 
         </b-navbar-nav>
       </b-collapse>
@@ -31,11 +48,37 @@ export default {
 
 <style scoped> 
 
-.company-text{
+.navbar-text{
     font-size: 24px;
+    color: white !important;
+
+}
+
+
+.right-navbar-content > li{
+  padding: 10px;
+}
+
+.right-nav-items{
+  display: flex;
+  flex-direction: row;
+}
+
+.right-nav-items > ul{
+  padding: 10px;
 }
 
  .navbar.navbar-dark.bg-dark{
     background-color: transparent !important;
  }
+
+.company-text{
+  padding-left: 100px;
+}
+
+#open-now{
+  font-size: 14px;
+}
+
+ 
 </style>
